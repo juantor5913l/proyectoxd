cfd3f0168498dd5fd724e2241b7d7b3c4077db97
Mariale051 <128168046+MiguelQuiroga2412@users.noreply.github.com>
2023-08-31 17:28:08 -0500

    vistas

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 #dependencia de flask
-from flask import Flask
+from flask import Flask, render_template,url_for
 
 #Dependencia de configuracion 
 from .config import Config #El punto es para indicarle a python que los archivos estan en el mismo paquete
@@ -10,30 +10,33 @@ from flask_sqlalchemy import SQLAlchemy
 #dependencia de las migraciones
 from flask_migrate import Migrate
 
-
-
-
 #crear el objeto flask
 app = Flask(__name__)
 
 #Configuracion del objeto flask
 app.config.from_object(Config)
 
-
-
 #Importar el modulo 
 from app.materiales import materiales_blueprint
 from app.ordenes import ordenes_blueprint
-from app.index import index_blueprint
+
 
 #Vincular submodulos del proyecto
 app.register_blueprint(materiales_blueprint)
 app.register_blueprint(ordenes_blueprint)
-app.register_blueprint(index_blueprint)
+
 #crear el objeto de modelos:
 
 db=SQLAlchemy(app)
 #crear el obejeto de migracion 
 migrate=Migrate(app,db)
+
+
+
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html' )
 
 from .models import RolUsuario,Usuario,Material,Cotizacion,OrdenServicio
--- app/index/__init__.py
+++
@@ -1,7 +0,0 @@
-#Dependencia para hacer un blueprint/paquete
-from flask import Blueprint
-
-#Definir paquete de productos
-index_blueprint = Blueprint ('index_blueprint', __name__, url_prefix ='/index', template_folder = 'templates')
-
-from . import routes
